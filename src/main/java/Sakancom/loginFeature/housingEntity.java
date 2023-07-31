1a4653565c4fb54e88e21a2ad5f80a727b4ce04d
YazanM23 <112900360+YazanM23@users.noreply.github.com>
2023-07-31 22:49:59 +0300

    Update housingEntity.java

--- src/main/java/Sakancom/loginFeature/housingEntity.java
+++ src/main/java/Sakancom/loginFeature/housingEntity.java
@@ -1,44 +1,47 @@
-package Sakancom.loginFeature;
+package sakancom.loginFeature;
 
-import java.io.Console;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
-public class housingEntity {
-    String Picture, Price, Location, Services, Available, Owner, Tenant, ID;
+public class HousingEntity {
+    String host = "localhost";
+    int port = 3306;
+    String database = "sakancom";
+    String username = "root";
+    String password = "password";
+    String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+    Logger logger = Logger.getLogger(HousingEntity.class.getName());
+String wid="' where id='";
 
     public int showAvailable() {
-        String host = "localhost";
-        int port = 3306;
-        String database = "Sakancom";
-        String username = "root";
-        String password = "password";
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+
         int counter = 1;
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Connected to the MySQL database!");
+
             Statement statement = connection.createStatement();
-            String query = new String();
-            query = "Select * from housing where available='Yes' and accept='true'";
+
+           String query = "Select * from housing where available='Yes' and accept='true'";
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
 
-                System.out.println(counter + "-");
-                System.out.println("ID : " + resultSet.getString(8));
-                System.out.println("Picture : " + resultSet.getString(1));
-                System.out.println("Price : " + resultSet.getString(2));
-                System.out.println("Location : " + resultSet.getString(3));
-                System.out.println("Services : " + resultSet.getString(4));
-                System.out.println("Number of people lived in : " + resultSet.getString(11));
-                System.out.println("Floor Number: " + resultSet.getString(9));
-                System.out.println("Department Name : " + resultSet.getString(10));
+
+                logger.info(counter + "-");
+                logger.info("ID : {}" +resultSet.getString(8) );
+                logger.info("Picture : {}" + resultSet.getString(1));
+                logger.info("Price : {}" + resultSet.getString(2));
+                logger.info("Location : {}" + resultSet.getString(3));
+                logger.info("Services : {}" + resultSet.getString(4));
+                logger.info("Number of people lived in : {}" + resultSet.getString(11));
+                logger.info("Floor Number: {}" + resultSet.getString(9));
+                logger.info("Department Name : {}" + resultSet.getString(10));
 
 
                 counter++;
-                System.out.println("_____________________________________________");
+                logger.info("_____________________________________________");
 
             }
         } catch (Exception e) {
@@ -48,18 +51,13 @@ public class housingEntity {
     }
 
     public boolean availableWithID(String id){
-        String host = "localhost";
-        int port = 3306;
-        String database = "Sakancom";
-        String username = "root";
-        String password = "password";
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+
         int counter = 1;
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Connected to the MySQL database!");
+
             Statement statement = connection.createStatement();
-            String query = new String();
-            query = "Select * from housing where id='"+id+"' and available='Yes'";
+
+            String query = "Select * from housing where id='"+id+"' and available='Yes'";
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
@@ -85,21 +83,14 @@ public class housingEntity {
 
                 }
                 if (flag1 == 1 && id.isEmpty() == false) {
-                    int flagg = 0;
-                    String host = "localhost";
-                    int port = 3306;
-                    String database = "Sakancom";
-                    String username = "root";
-                    String password = "password";
-                    String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
-                    int counter = 1;
+
                     try (Connection connection = DriverManager.getConnection(url, username, password)) {
-                        System.out.println("Connected to the MySQL database!");
+
                         Statement statement = connection.createStatement();
                         Statement statement1 = connection.createStatement();
                         Statement statement2 = connection.createStatement();
-                        String query = new String();
-                        query="select * from housing where id='" + id + "'and available ='Yes'";
+
+                       String query="select * from housing where id='" + id + "'and available ='Yes'";
                         ResultSet res=statement.executeQuery(query);
 
                        while(res.next()){
@@ -109,12 +100,12 @@ public class housingEntity {
                                num++;
                                String n=Integer.toString(num);
                                if(num==4){
-                                   query="update housing set peopleNum='"+n+"',available='No',tenant='"+userName+"' where id='"+id+"'";
+                                   query="update housing set peopleNum='"+n+"',available='No',tenant='"+userName+wid+id+"'";
                                    statement1.executeUpdate(query);
 flag=1;
 
                                }else{
-                                   query="update housing set peopleNum='"+n+"',tenant='"+userName+"' where id='"+id+"'";
+                                   query="update housing set peopleNum='"+n+"',tenant='"+userName+wid+id+"'";
                                    statement1.executeUpdate(query);
                                    flag=1;
                                }
@@ -127,7 +118,7 @@ flag=1;
                            }
                            else{
 
-                                 query = "update housing set available='No',peopleNum='+"+num+"',tenant='"+userName+"' where id='" + id + "'";
+                                 query = "update housing set available='No',peopleNum='+"+num+"',tenant='"+userName+wid + id + "'";
                                statement1.executeUpdate(query);
                                flag=1;
                            }
