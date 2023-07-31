407b1d3c85442388c8699b14ca04716994e322b5
YazanM23 <112900360+YazanM23@users.noreply.github.com>
2023-07-31 23:35:28 +0300

    Update showLivedIn.java

--- src/main/java/Sakancom/loginFeature/showLivedIn.java
+++ src/main/java/Sakancom/loginFeature/showLivedIn.java
@@ -1,27 +1,26 @@
-package Sakancom.loginFeature;
-
-import io.cucumber.java.an.E;
+package sakancom.loginFeature;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
-public class showLivedIn {
-
+public class ShowLivedIn {
+    String host = "localhost";
+    int port = 3306;
+    String database = "sakancom";
+    String username = "root";
+    String password = "password";
+    String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+    private static final Logger logger = Logger.getLogger(ShowLivedIn.class.getName());
     public boolean isLived(String id){
-        String host = "localhost";
-        int port = 3306;
-        String database = "Sakancom";
-        String username = "root";
-        String password = "password";
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
 
             Statement statement = connection.createStatement();
-            String query = new String();
-            query = "Select * from Tenants_Housing where houseID='" + id + "'";
+
+           String query = "Select * from Tenants_Housing where houseID='" + id + "'";
             ResultSet res = statement.executeQuery(query);
             while (res.next()){
                 return true;
@@ -37,21 +36,15 @@ public class showLivedIn {
     }
     public boolean displayLived(String id) {
         if (isLived(id)) {
-            String host = "localhost";
-            int port = 3306;
-            String database = "Sakancom";
-            String username = "root";
-            String password = "password";
-            String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
 
             try (Connection connection = DriverManager.getConnection(url, username, password)) {
 
                 Statement statement = connection.createStatement();
-                String query = new String();
-                query = "Select * from Tenants_Housing where houseID='" + id + "'";
+                
+               String query = "Select * from Tenants_Housing where houseID='" + id + "'";
                 ResultSet res = statement.executeQuery(query);
                 while (res.next()) {
-                    System.out.println("People is : " + res.getString(1));
+                    logger.info("People is : " + res.getString(1));
 
                 }
                 return true;
